ce648e6534d273cec2dbdb9abd290b3a25122fce
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-02 20:07:02 +0100

    Variables

---
+++ ageChecker.js
@@ -0,0 +1,27 @@
+//Create a conditional that checks their age
+//If under 16, tell them they can not drive
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+//If under 21, tell them they can not drink
+//If under 25, tell them they can not rent cars affordably
+//If under 30, tell them they can not rent fancy cars affordably
+//If under over 30, tell them there is nothing left to look forward too
+
+let age = 10
+if (age < 16){
+    console.log ("YOU CANNOT DRIVE")
+} else if (age < 18) {
+    console.log ("YOU CAN'T HATE FROM OUTSIDE THE CLUB BECAUSE YOU CANNOT GET IN")
+}else if (age < 21) {
+    console.log ("YOU CANNOT DRINK")
+} else if (age < 25) {
+    console.log ("YOU CANNOT RENT FANCY CARS AFFORDABLY")
+} else if (age < 30) {
+    console.log ("YOU CANNOT RENT FANCY CARS AFFORDABLY")
+} else if (age > 30) {
+    console.log ("THERE'S NOTHING LEFT TO LOOK FORWARD TOO")
+}
+//--- Harder
+//On click of the h1
+//Take the value from the input
+//Place the result of the conditional in the paragraph
+
