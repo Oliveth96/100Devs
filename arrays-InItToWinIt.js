8b1e4d7e93e9bb007ee11da86c525aa0f0960652
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-14 20:38:44 +0100

    First attempt at array

---
+++ arrays-InItToWinIt.js
@@ -0,0 +1,18 @@
+//Create an array of movies with at least three movies.
+
+movies = ['jumong','vincenzo','Empress ki']
+
+//Using the array from above, store the first movie in a variable
+
+let movies = ['jumong','vincenzo','Empress ki']
+let kDrama = movies[0]
+
+
+//Get the length of the original array and store it in a new variable
+
+let KoreanMovies = movies.length
+console.log(KoreanMovies)
+
+//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+
+let myMovies = movies[2]
