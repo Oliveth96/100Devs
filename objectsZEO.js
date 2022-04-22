f31af636c473ec8c13a137157c74b3d37d54f59c
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-22 03:57:57 +0100

    Objects in Javascript

--- objectsZEO.js
+++ objectsZEO.js
@@ -1,7 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
 let stopwatch = {}
 
 stopwatch.color = 'black'
 stopwatch.shape = 'round'
 stopwatch.currentTime = 3
-stopwatch.brandName = 'casio'
+stopwatch.brandName = 'Casio'
+
+stopwatch.start = function(){
+    console.log('STARTTTTTTIIIINNNNGGG')
+}
+
+stopwatch.stop = function(){
+    console.log('STOOOOPPPPING')
+}
+
+stopwatch.sayBrand = function(){
+    console.log( stopwatch.brandName )
+}
