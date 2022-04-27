835c1628af47b99f250e1873deb5c9eef8a3345a
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-04-27 08:36:40 +0100

    Receive a new dog image upon every refresh

--- dogPhotos.js
+++ dogPhotos.js
@@ -4,6 +4,7 @@ fetch("https://dog.ceo/api/breeds/image/random")
 .then(res => res.json()) //parse response as JSON
 .then(data => {
     console.log(data.message)
+    document.querySelector('img').src = data.message  //This take the message we receive from the api and send to the DOM everytime we refresh the page
 })
 .catch (err => {
     console.log('error ${err}')
