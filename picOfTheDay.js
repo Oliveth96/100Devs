b193f89fae3d93c786194a16e8ec69e521a2c62b
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-05-07 15:50:34 +0100

    NASA API

--- picOfTheDay.js
+++ picOfTheDay.js
@@ -1,16 +1,19 @@
-//Example fetch using pokemonapi.com
+//Example fetch using api.nasa.gov
 
 // A clickEvent that calls the function getFetch. little smurfs
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice // ${choice}
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://api.nasa.gov/planetary/apod?api_key=KJcxIp1daSZbXs3bEZMfFbguW15DTGLrwlUabX1E&date=${choice}`
   
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
