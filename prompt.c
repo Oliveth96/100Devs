8101a30f00097f95aee3a0dbd27d57d5e649683e
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-05-14 17:31:05 +0100

    Writes a program that prints '$', waits for the user to enter a command, prints it on the next line.

---
+++ prompt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+    int i;
+    char *av[];
+    av[] = malloc(sizeof(char *) * i);
+
+    for (i = 0; i < av[i] != '\0'; i++)
+    {
+        printf("%s", av[]);
+    }
+    return(0);
+}
