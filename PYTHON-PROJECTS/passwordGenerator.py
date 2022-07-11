a15ec6e1d91664fca9b6720f16fa469f575a26f2
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-07-11 13:33:40 +0100

    A script that generates alpha numeric passwords

---
+++ PYTHON-PROJECTS/passwordGenerator.py
@@ -0,0 +1,21 @@
+import secrets
+import string
+'''
+Generates a ten-character alphanumeric password
+'''
+alphabet = string.ascii_letters + string.digits
+password = "".join(secrets.choice(alphabet) for i in range(10))
+print(password)
+
+'''
+Generates a 10 character alpha-numeric password with at least
+1 lowercase character, at least 1 upper case character
+and at least three digits
+'''
+anotherAlphabet = string.ascii_letters + string.digits
+while True:
+    aPassword = ''.join(secrets.choice(anotherAlphabet) for i in range(20))
+    if (any(c.islower() for c in aPassword) and any(c.isupper()
+    for c in aPassword) and sum(c.isdigit() for c in aPassword) >= 3):
+        print(aPassword)
+        break
