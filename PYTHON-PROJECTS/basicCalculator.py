89d21c98ceb4918c61e15ca62702b3f1a7028bc2
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-07-11 18:29:42 +0100

    A caculator that solves LCM functions

--- PYTHON-PROJECTS/basicCalculator.py
+++ PYTHON-PROJECTS/basicCalculator.py
@@ -34,3 +34,23 @@ if (operator == '//'):
 if (operator == '**'):
     Result = firstOperand ** secondOperand
 print('The answer = ', Result)
+
+
+"""
+Module providing Lease common Multiples
+"""
+
+def leastCommonMultiple(a, b):
+    if a > b:
+        greater = a
+    elif b > a:
+        greater = b
+    while(True):
+        if ((greater % a == 0) and (greater % b == 0)):
+            lcm = greater
+            break
+        greater = greater + 1
+    return lcm
+a = int(input("Please enter first number: "))
+b = int(input("Please enter second number: "))
+print(leastCommonMultiple(a, b))
