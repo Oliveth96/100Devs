fc1e8048aa0f65821eb16aa732552f4d28bf5734
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-07-11 13:16:50 +0100

    A script that takes screenshots and saves it

---
+++ PYTHON-PROJECTS/screenshots.py
@@ -0,0 +1,8 @@
+#import Library
+import pyautogui
+
+#Take screenshot
+myScreenshot=pyautogui.screenshot()
+
+#Set a path/directory to save screenshot images.
+myScreenshot.save(r'C:\\Users\\HP\\OneDrive\\Pictures\\online.png')
