76161d919060f7eac57210e18e0be7721ea18924
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-07-12 13:16:01 +0100

    Palindrom check on words

--- PYTHON-PROJECTS/palindromeSentence.py
+++ PYTHON-PROJECTS/palindromeSentence.py
@@ -11,3 +11,26 @@ def palindromeWords(sentence):
 
 sentence = input("Please enter any sentence : ")
 print(palindromeWords(sentence))
+
+
+
+
+
+"""
+Module that provides palindrome checks on strings
+"""
+
+
+def palindrome(string):
+    """
+    A function that checks if the string is a palindrome.
+    """
+    revereseString = string[::-1]
+    if string == revereseString:
+        print("This Word is a palindrome")
+    else:
+        print('This Word is not a palindrome')
+
+if __name__ == '__main__':
+    messagePrompt = str(input("Please enter a word to check if it is a palindrome: "))
+    palindrome(messagePrompt)
